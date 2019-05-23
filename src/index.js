8dc24a90e051334654b984f0e4c1b5cad778c738
singhcoolsanjay <singhcoolsanjay@gmail.com>
2019-05-23 16:29:53 +0530

    changes for search

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './App'
+import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(<App/> , document.getElementById('root'));
+
+
+serviceWorker.unregister();
